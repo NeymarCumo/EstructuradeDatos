c64bf5f8bde0eba47a0175c57c59e308c9a26a8d
Giovanni-Cumo <cumoreyesgiovanni7@gmail.com>
2024-09-25 12:04:04 -0600

    primer commit

---
+++ Semana4/homework/Semana5/variables.js
@@ -0,0 +1,45 @@
+//Ejercicio de variable de js"
+
+//Declaracion de variable con var//
+
+//variable de tipo string no hay diferencia si uso comilla simple o //
+var nombre = "felipe"
+var ciudad = "Ajalpan"
+
+//Variable de tipo numero
+var entero = 60;
+var negativo = -120;
+var decimal = 10.67;
+
+//variable de tipo boleano
+var verdad = true;
+var mentira = false;
+
+//variable de tipo Undifine
+var noseeltipo;
+
+//Variable Null
+var nula = null
+
+//operadores matematicos
+//suma
+console.log(50 + 5)
+var num1 = 50;
+console.log(num1 + 5)
+//resta
+var num2 = 20;
+console.log(num1 - num2)
+//division
+console.log(250/num1)
+console.log(num1/num2)
+//modulo
+console.log(num1%num2)
+//metodo de procedencia
+// *, /, + y -//
+console.log(3 + 5 * 2 - 8 * 5 / 2);
+console.log(3 / 2 + 5 * 4 - 3 + 100 / 2 * 4);
+// 1.5 + 20 - 3 + 50 * 4
+// 1.5 + 20 - 3 + 200
+// 21.5- 3 + 200
+// 21.5 - 197
+// 218.5
