4b4aa22fe082c36498a41954e8a84f7afc0e2c6c
Giovanni-Cumo <cumoreyesgiovanni7@gmail.com>
2024-10-25 22:28:15 -0600

    Viernes25 Octubre

---
+++ Semana de culminacion de henry/objetos.js
@@ -0,0 +1,71 @@
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+ };
+ var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+ // ACCEDER
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
+
+//Dot-Notation y Bracket-Notation
+
+// DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+//Objetos avanzados
+
+// HAS OWN PROPERTY
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+
+for (let prop in mundo) {
+    console.log('Esta es la propiedad: ', prop);
+    console.log('Este es el valor: ', mundo[prop]);
+ }
+
+ //THIS
+var mascota = {
+    animal: 'Perro',
+    raza: 'Ovejero Alemán',
+    amistoso: true,
+    dueño: 'María López',
+    info: () => {
+       console.log('Mi perro es un  ' + this.raza);
+    },
+ };
+
+ //Callbacks
+ //Las funciones callback son un tipo de funciones que se pasan por parámetro a otras funciones, en esta video clase aprenderemos que son y cómo usarlas a través de algunos ejemplos.
+ function devuelvoUsuario() {
+    return 'CAMILO';
+ }
+ function devuelvoSaludo() {
+    return 'Hola';
+ }
+ function saludar(cb1, cb2) {
+    return cb1() + ' ' + cb2();
+ }
+ var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
+ console.log(resultado);
